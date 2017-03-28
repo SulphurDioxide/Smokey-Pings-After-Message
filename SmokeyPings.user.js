7aee70d150ed1c25abc3a6291d3fcb62c557ded9
SulphurDioxide <git@adhenderson.co.uk>
2017-03-28 16:45:32 +0100

    Tidies up and organises IDs by name
    Commented this better. I've also taken out the explicit reference to
    Smokey's SO ID and replaced it with a variable so we can do cleverer
    stuff with it in the future.

--- SmokeyPings.user.js
+++ SmokeyPings.user.js
@@ -4,15 +4,38 @@
 // @author      Henders
 // @attribution Andy Henderson (https://github.com/SulphurDioxide)
 // @version     0.0.1
+// @match       *://chat.stackexchange.com/rooms/11540/charcoal-hq*
 // @match       *://chat.stackoverflow.com/rooms/41570/so-close-vote-reviewers*
 // ==/UserScript==
-console.log('Smokey Pings ran!');
-$('#chat').bind('DOMNodeInserted', function(){ 
-    //if($('div.user-container:last-child.user-3735529:not(.notified)').length > 0){ 
-    if($('div.user-container .user-3735529:not(.notified)').length > 0){ 
-        $('#jp_audio_0')[0].play(); 
-        //$('div.user-container:last-child.user-3735529:not(.notified)').addClass('notified');
-        $('div.user-container .user-3735529:not(.notified)').addClass('notified');
+
+// A list of IDs that can be chosen to follow:
+var smokeySO = 3735529;
+var smokeySE = 120914;
+var henders = 2233391;
+
+/*
+    If you want to use smokey on all chat rooms set the userID as 'smokeyALL' and it will select the appropriate smokey ID for the chatroom you are in.
+*/
+var userID = "smokeyALL";
+
+// Out of all of the listed IDs, which one shall we use?
+if(userID == 'smokeyALL'){
+    if(window.location.href.indexOf('41570/so-close-vote-reviewers') > 0){
+        var userID = smokeySO;
+    } else {
+        var userID = smokeySE;
+    }
+}
+
+// Binds the event 'DOMNodeINserted' to the element with the 'chat' id.
+$('#chat').bind('DOMNodeInserted', function(){
+	// If there is a div with the following classes (userID specifying the user this applies to)
+    if($('div.user-container:last-child.user-'+userID+':not(.notified)').length > 0){
+    	// Play the chatroom's notification sound
+        $('#jp_audio_0')[0].play();
+        // Add a class 'notified' to the item so there are not continuous notifications about the same exact item.
+        $('div.user-container:last-child.user-'+userID+':not(.notified)').addClass('notified');
+
         console.log('notified');
     }
 });
