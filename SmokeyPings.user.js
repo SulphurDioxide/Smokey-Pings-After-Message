3341f26a18ce4b53bea6bf1c1daef2be41d0927a
SulphurDioxide <git@adhenderson.co.uk>
2017-09-01 21:25:44 +0100

    Adds option to choose notification sound

--- SmokeyPings.user.js
+++ SmokeyPings.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Smokey Pings After Message (SPAM)
-// @description Play the ping sound if Smokey has a message at the bottom of the SOCVR or Charcoal HQ chats.
+// @description Play the ping sound if Smokey has a message at the bottom of the SOCVR chat.
 // @author      Henders
 // @attribution Andy Henderson (https://github.com/SulphurDioxide)
 // @version     0.0.4
@@ -16,6 +16,16 @@ const smokeyID = {
 	'chat.stackexchange.com':120914,
 	'chat.stackoverflow.com':3735529,
 }[window.location.host];
+
+// If there is no sound set, set the default one:
+if(localStorage.getItem('SPAM-settings') == null){
+  // Set the default value:
+  var defaultSpamSettings = {
+    'notificationSound':'default',
+  };
+  
+  localStorage.setItem('SPAM-settings', JSON.stringify(defaultSpamSettings));
+}
 
 /*
     One day this will be selectable so you can choose which ID you want notifying about. 
@@ -33,6 +43,15 @@ var reportRegex = /\[ <a[^>]+>SmokeDetector<\/a> \| <a[^>]+>MS<\/a> ] /;
 // Add our function to the CHAT event handler: 
 CHAT.addEventHandlerHook(chatMessageRecieved);
 
+// Add the SPAM link to change the settings:
+$('#sidebar-menu').append("| <a id='spamOptions' href='javascript:void(0);' onclick=''>SPAM</a>");
+$('head').append("<style> .spamSettings { position: fixed; z-index: 50; padding: 10px; border: 1px solid #aaa; width: 200px; font-size: 11px; color: #444; -moz-border-radius: 5px; -webkit-border-radius: 5px; border-radius: 5px; background: rgba(255,255,255,0.95); filter: alpha(opacity=95); -webkit-box-shadow: 0 1px 15px #9c9c9c; -moz-box-shadow: 0 1px 15px #9c9c9c; box-shadow: 0 1px 15px #9c9c9c; overflow: hidden; } </style>");
+
+$('#jplayer').append('<audio id="jp_audio_clavinova" preload="metadata" src="https://adhenderson.co.uk/sounds/clavinova.mp3"></audio><audio id="jp_audio_vibraphone" preload="metadata" src="https://adhenderson.co.uk/sounds/vibraphone.mp3"></audio><audio id="jp_audio_piano" preload="metadata" src="https://adhenderson.co.uk/sounds/piano.mp3"></audio>');
+
+$('#spamOptions').click(function(){
+   toggleSpamOptions();
+});
 /*
     This function is called when a new chat event fires. 
     
@@ -60,7 +79,59 @@ function chatMessageRecieved({event_type, user_id, content}){
             }
         }
         // Play the ping sound:
-        $('#jp_audio_0')[0].play();
+        playSpamSound();
         
     }
+}
+
+/*
+    This function shows / hides the spam settings menu.
+*/
+function toggleSpamOptions(){
+    var spamOptionsMenu = "<div id='spamOptionsMenu' class='spamSettings'><div style='position: absolute; right: 10px;'><a id='closeSpamOptions' href='javascript:void(0);'>Close</a></div><h2>Spam Options</h2><strong>Notification Sound</strong>:<div><select id='spamSoundSelect'><option value='default'>default</option><option value='piano'>Piano</option><option value='clavinova'>Clavinova</option><option value='vibraphone'>Vibraphone</option></select></div> </div>";
+    
+    if($('#spamOptionsMenu').length < 1){
+      $('#spamOptions').after(spamOptionsMenu);
+      $('#closeSpamOptions').click(function(){
+        toggleSpamOptions();
+      });
+      $('#spamSoundSelect').change(function(){
+        playSpamSound(this.value);
+        setSpamSound(this.value);
+      });
+    } else {
+     // Get rid of the menu if it already exists:
+     $('#spamOptionsMenu').remove();
+    }
+}
+
+function setSpamSound(sound){
+  // Current stored settings:
+  var currentStoredSettings = JSON.parse(localStorage.getItem('SPAM-settings'));
+  
+  currentStoredSettings.notificationSound = sound;
+  
+  localStorage.setItem('SPAM-settings', JSON.stringify(currentStoredSettings));
+
+}
+
+function playSpamSound(sound){
+  const soundMap = {
+    'default':'jp_audio_0',
+    'clavinova':'jp_audio_clavinova',
+    'piano':'jp_audio_piano',
+    'vibraphone':'jp_audio_vibraphone',
+  };
+  
+  if(typeof sound == 'undefined'){
+    // Play the stored sound:
+    var storedSound = JSON.parse(localStorage.getItem('SPAM-settings')).notificationSound;
+    
+    $('#'+soundMap.storedSound)[0].play();
+    
+  } else {
+    // Play the sound specified:
+    $('#'+soundMap[sound])[0].play();
+    
+  }
 }
